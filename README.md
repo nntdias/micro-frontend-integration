e6da6e3bcdc7643777cc907176739b77037e1f90
Nonato Dias <nonato.developer@gmail.com>
2021-03-08 17:51:06 -0300

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # micro-frontend-integration
+
+An example of implementing Module Federation.
+
+## Installation
+
+This project has been designed for gradual adoption from the start, adding modules and using them.
+
+To run it in development mode, you need to go into the folders inside **packages** and run `npm install`.
+After that, run `npm start`.
+
+## Documentation
+
+You can find the Module Federation documentation [on the website](https://webpack.js.org/concepts/module-federation/).
--- packages/container/src/views/auth/login/LoginView.js
+++ packages/container/src/views/auth/login/LoginView.js
@@ -3,7 +3,7 @@ import React, { useCallback, useEffect, useRef } from 'react';
 /**
  * LoginView
  */
-const LoginView = () => {
+const LoginView = props => {
 	const ref = useRef(null);
 	const refMount = useRef(null);
 
@@ -11,8 +11,8 @@ const LoginView = () => {
 		if (!refMount.current) {
 			refMount.current = await import('auth/AuthApp').then(({ mount }) => mount);
 		}
-		refMount.current(ref.current);
-	}, []);
+		refMount.current(ref.current, props);
+	}, [props]);
 
 	useEffect(() => {
 		loadModule();
